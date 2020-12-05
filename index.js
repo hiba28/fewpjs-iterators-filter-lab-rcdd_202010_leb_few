901c5671afc5e2306aeaad04d601f44569afcbcc
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-05 11:19:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching (list, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+function fuzzyMatch(drivers,string) {
+  let sameLetter = drivers.filter(name => name.charAt(0) === string.charAt(0))
+  return sameLetter
+}
+function matchName(drivers,string) {
+  let matchName = drivers.filter(elem => elem.name === string)
+  return matchName
+}
+)
